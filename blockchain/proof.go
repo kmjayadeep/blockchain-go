aa211883458187320e58211c7ac07685ece8c7f1
Jayadeep KM <kmjayadeep@gmail.com>
2021-12-27 21:15:13 +0530

    proof start
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,1 +1,23 @@
 package blockchain
+
+import "math/big"
+
+const Difficulty = 12
+
+type ProofOfWork struct {
+	Block  *Block
+	Target *big.Int
+}
+
+func NewProof(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+
+	target.Lsh(target, uint(256-Difficulty))
+
+	pow := &ProofOfWork{
+		b,
+		target,
+	}
+
+	return pow
+}
--- blockchain/proof_test.go
+++ blockchain/proof_test.go
@@ -4,6 +4,15 @@ import (
 	"testing"
 )
 
-func TestProof(t *testing.T) {
+func TestNewProof(t *testing.T) {
+	pow := NewProof(CreateBlock("test", []byte("test")))
+
+	if pow.Target == nil {
+		t.Errorf("target not defined")
+	}
+
+	if pow.Target.String() != "28269553036454149273332760011886696253239742350009903329945699220681916416" {
+		t.Errorf("target is invalid: got : %s", pow.Target)
+	}
 
 }
