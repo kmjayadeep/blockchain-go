a2bbbf14df90c9d9726761d62ab12fe6f7aea16e
Jayadeep KM <kmjayadeep@gmail.com>
2021-12-27 21:26:37 +0530

    proof of work
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,23 +1,10 @@
 package blockchain
 
-import (
-	"bytes"
-	"crypto/sha256"
-)
-
 type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join(
-		[][]byte{b.Data, b.PrevHash},
-		[]byte{},
-	)
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
+	Nonce    int
 }
 
 func CreateBlock(data string, prevHash []byte) *Block {
@@ -25,8 +12,14 @@ func CreateBlock(data string, prevHash []byte) *Block {
 		[]byte{},
 		[]byte(data),
 		prevHash,
+		0,
 	}
-	block.DeriveHash()
+
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
 
--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,6 +1,13 @@
 package blockchain
 
-import "math/big"
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"log"
+	"math"
+	"math/big"
+)
 
 const Difficulty = 12
 
@@ -20,4 +27,49 @@ func NewProof(b *Block) *ProofOfWork {
 	}
 
 	return pow
+}
+
+func (pow *ProofOfWork) InitData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.Block.PrevHash,
+			pow.Block.Data,
+			ToHex(int64(nonce)),
+			ToHex(int64(Difficulty)),
+		},
+		[]byte{},
+	)
+	return data
+}
+
+func ToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return buff.Bytes()
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var intHash big.Int
+	var hash [32]byte
+
+	nonce := 0
+
+	for nonce < math.MaxInt64 {
+		data := pow.InitData(nonce)
+		hash = sha256.Sum256(data)
+		intHash.SetBytes(hash[:])
+
+		if intHash.Cmp(pow.Target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+
+	}
+
+	return nonce, hash[:]
 }
