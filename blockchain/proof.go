ca750ffe46278055ac77d926ed94147d73e43865
Jayadeep KM <kmjayadeep@gmail.com>
2021-12-27 23:00:22 +0530

    proof validate
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -73,3 +73,14 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 
 	return nonce, hash[:]
 }
+
+func (pow *ProofOfWork) Validate() bool {
+	var intHash big.Int
+
+	data := pow.InitData(pow.Block.Nonce)
+
+	hash := sha256.Sum256(data)
+	intHash.SetBytes(hash[:])
+
+	return intHash.Cmp(pow.Target) == -1
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/kmjayadeep/blockchain-go/blockchain"
 )
@@ -17,6 +18,11 @@ func main() {
 	for _, block := range chain.Blocks {
 		fmt.Printf("Data %s\n", block.Data)
 		fmt.Printf("hash %x\n", block.Hash)
+		fmt.Printf("nonce %d\n", block.Nonce)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("pow validated %s\n", strconv.FormatBool(pow.Validate()))
+
 		fmt.Printf("\n\n")
 	}
 
