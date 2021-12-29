1e26a36b0aaeaae61a566ef9c497b20225aea4e0
Jayadeep KM <kmjayadeep@gmail.com>
2021-12-29 19:38:32 +0530

    blockchain pkg tests fix
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- blockchain/blockchain_test.go
+++ blockchain/blockchain_test.go
@@ -1,4 +1,4 @@
-package blockchain
+package blockchain_test
 
 import (
 	"fmt"
@@ -6,9 +6,22 @@ import (
 
 	"github.com/dgraph-io/badger/v3"
 	"github.com/kmjayadeep/blockchain-go/block"
+	"github.com/kmjayadeep/blockchain-go/blockchain"
 	"github.com/kmjayadeep/blockchain-go/storage"
 	"github.com/kmjayadeep/blockchain-go/transaction"
 )
+
+var (
+	GenesisData = "First transaction from Genesis"
+)
+
+func createTestTransaction(id string) *transaction.Transaction {
+	return &transaction.Transaction{
+		ID:      []byte(id),
+		Inputs:  []transaction.TxInput{},
+		Outputs: []transaction.TxOutput{},
+	}
+}
 
 func TestInitBlockChain(t *testing.T) {
 	db, err := storage.NewDatabase(badger.DefaultOptions("").WithInMemory(true))
@@ -16,7 +29,7 @@ func TestInitBlockChain(t *testing.T) {
 		t.Errorf("unable to initialize db")
 	}
 	defer db.Close()
-	chain, err := InitBlockChain(db, "myaddress")
+	chain, err := blockchain.InitBlockChain(db, "myaddress")
 
 	if chain == nil {
 		t.Errorf("unable to initialize chain")
@@ -26,7 +39,7 @@ func TestInitBlockChain(t *testing.T) {
 		t.Errorf("unable to initialize chain with error %s", err.Error())
 	}
 
-	genesisHash := "00031a02a972efd4fa6ea999407149b85b03ccecb8c2bb8eb5a1d068862309d0"
+	genesisHash := "0001e82dbba3246cc5693afda356020b6263ad0932ff8344cb728941dacdbd3c"
 	if fmt.Sprintf("%x", chain.LastHash) != genesisHash {
 		t.Errorf("blockchain doesn't have genesis, got hash %x", chain.LastHash)
 	}
@@ -56,12 +69,16 @@ func TestAddBlock(t *testing.T) {
 		t.Errorf("unable to initialize db")
 	}
 	defer db.Close()
-	chain, err := InitBlockChain(db, "address")
+
+	address := "address"
+
+	chain, err := blockchain.InitBlockChain(db, address)
 	if err != nil {
 		t.Errorf("unable to initialize chain with error %s", err.Error())
 	}
 
-	err = chain.AddBlock("testing")
+	tx := createTestTransaction("testing")
+	err = chain.AddBlock([]*transaction.Transaction{tx})
 	if err != nil {
 		t.Errorf("unable to add block with error %s", err.Error())
 	}
@@ -79,13 +96,13 @@ func TestAddBlock(t *testing.T) {
 		t.Errorf("blockchain count doesnt match. got size %d", len(blocks))
 	}
 
-	tx, _ := transaction.CoinbaseTx("address", "genesis")
+	tx, _ = transaction.CoinbaseTx(address, GenesisData)
 	genesis := block.Genesis(tx)
 	if genesis.String() != blocks[1].String() {
 		t.Errorf("blockchain doesn't have genesis - %s,\n got %s", genesis, blocks[1])
 	}
 
-	if blocks[0].Transactions[0].Inputs[0].Sig != "testing" {
-		t.Errorf("blockchain doesn't have new block, got data %s", blocks[0].Transactions[0].Inputs[0].Sig)
+	if blocks[0].String() != "Block - Hash:00079b63d0bc0e4ff0938996d27f729f37588a6ac44a9fcecb74e83fdd84579f, PrevHash:0004e6cfb234624214b26607c1a6a241ec43d66f1245b23467e3be27c3cad556, Transactions:1, Nonce:1492" {
+		t.Errorf("blockchain doesn't have new block, got data %s", blocks[0].String())
 	}
 }
