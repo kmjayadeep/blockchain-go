1024a5a6f0c3d6de55117d92c74914a72877a3e6
Jayadeep KM <kmjayadeep@gmail.com>
2022-01-01 10:20:28 +0530

    transaction tests for coinbase
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- go.mod
+++ go.mod
@@ -12,12 +12,15 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/golang/groupcache v0.0.0-20190702054246-869f871628b6 // indirect
-	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/google/flatbuffers v1.12.1 // indirect
 	github.com/klauspost/compress v1.12.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
 	go.opencensus.io v0.22.5 // indirect
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
-	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- transaction/transaction_test.go
+++ transaction/transaction_test.go
@@ -36,10 +36,64 @@ func TestTransaction(t *testing.T) {
   checkEqual(t, inputs[1].Out, 11)
 
   outputs := tx.Outputs
+  checkEqual(t, len(outputs), 2)
   checkEqual(t, outputs[0].PubKey, to)
   checkEqual(t, outputs[1].PubKey, from)
   checkEqual(t, outputs[0].Value, 100)
   checkEqual(t, outputs[1].Value, 150)
+}
+
+func TestTransactionAmountValidation(t *testing.T) {
+  from := "abc"
+  to := "def"
+  amount := 100
+  accumulated := 25
+  validOutputs := map[string][]int{}
+
+  _, err := transaction.NewTransaction(from, to, amount, accumulated, validOutputs)
+
+  if err == nil {
+    t.Fatalf("transaction created")
+  }
+
+  checkEqual(t, err.Error(), "not enough funds")
+
+}
+
+
+func TestCoinbase(t *testing.T) {
+  tx, err := transaction.CoinbaseTx("toaddr", "testdata")
+  is := tx.IsCoinbase()
+
+  if err != nil {
+    t.Fatalf("transaction not created with eror %s", err.Error())
+  }
+  if tx.ID == nil || len(tx.ID) == 0 {
+    t.Errorf("Transaction id not set")
+  }
+
+
+  checkEqual(t, is, true)
+
+  inputs := tx.Inputs
+  checkEqual(t, len(inputs), 1)
+  checkEqual(t, inputs[0].Out, -1)
+  checkEqual(t, inputs[0].Sig, "testdata")
+
+  outputs := tx.Outputs
+  checkEqual(t, len(outputs), 1)
+  checkEqual(t, outputs[0].Value, 100)
+  checkEqual(t, outputs[0].PubKey, "toaddr")
+}
+
+func TestCoinbaseDefaultData(t *testing.T) {
+  tx, err := transaction.CoinbaseTx("toaddr", "")
+
+  if err != nil {
+    t.Fatalf("transaction not created with eror %s", err.Error())
+  }
+  inputs := tx.Inputs
+  checkEqual(t, inputs[0].Sig, "Coins to toaddr")
 }
 
 func checkDeepEqual(t *testing.T, expected, actual interface{}) {
