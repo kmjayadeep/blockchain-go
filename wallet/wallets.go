fc6d7e57dd1916f03eae2fad4249482f4351d9ab
Jayadeep KM <kmjayadeep@gmail.com>
2022-01-14 20:03:31 +0100

    fmt
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- wallet/wallets.go
+++ wallet/wallets.go
@@ -7,28 +7,28 @@ import (
 )
 
 type Wallets struct {
-  Wallets map[string]*Wallet
+	Wallets map[string]*Wallet
 }
 
 func NewWallets() *Wallets {
-  return &Wallets{
-    Wallets: make(map[string]*Wallet),
-  }
+	return &Wallets{
+		Wallets: make(map[string]*Wallet),
+	}
 }
 
 func LoadWallets(r io.Reader) (*Wallets, error) {
-  ws := &Wallets{}
-  gob.Register(elliptic.P256())
-  decoder := gob.NewDecoder(r)
-  err := decoder.Decode(ws)
-  if err != nil {
-    return nil, err
-  }
-  return ws, nil
+	ws := &Wallets{}
+	gob.Register(elliptic.P256())
+	decoder := gob.NewDecoder(r)
+	err := decoder.Decode(ws)
+	if err != nil {
+		return nil, err
+	}
+	return ws, nil
 }
 
 func (ws *Wallets) Save(w io.Writer) error {
-  gob.Register(elliptic.P256())
-  encoder := gob.NewEncoder(w)
-  return encoder.Encode(ws)
+	gob.Register(elliptic.P256())
+	encoder := gob.NewEncoder(w)
+	return encoder.Encode(ws)
 }
--- wallet/wallets_test.go
+++ wallet/wallets_test.go
@@ -9,33 +9,33 @@ import (
 )
 
 func TestWalletsStore(t *testing.T) {
-  b := bytes.Buffer{}
+	b := bytes.Buffer{}
 
-  ws := wallet.NewWallets()
-  w := wallet.Wallet{
-    PublicKey: []byte("test"),
-  }
-  ws.Wallets["test"] = &w
+	ws := wallet.NewWallets()
+	w := wallet.Wallet{
+		PublicKey: []byte("test"),
+	}
+	ws.Wallets["test"] = &w
 
-  err := ws.Save(&b)
-  if err != nil {
-    t.Errorf("Unable to save wallets with error %v", err)
-  }
+	err := ws.Save(&b)
+	if err != nil {
+		t.Errorf("Unable to save wallets with error %v", err)
+	}
 
-  loaded, err := wallet.LoadWallets(&b)
-  if err != nil {
-    t.Errorf("Unable to load wallets with error %v", err)
-  }
+	loaded, err := wallet.LoadWallets(&b)
+	if err != nil {
+		t.Errorf("Unable to load wallets with error %v", err)
+	}
 
-  if loaded == nil || loaded.Wallets == nil {
-    t.Fatalf("Wallets are nil")
-  }
+	if loaded == nil || loaded.Wallets == nil {
+		t.Fatalf("Wallets are nil")
+	}
 
-  if len(loaded.Wallets) == 0 {
-    t.Errorf("Wallets are empty")
-  }
+	if len(loaded.Wallets) == 0 {
+		t.Errorf("Wallets are empty")
+	}
 
-  if !reflect.DeepEqual(loaded, ws) {
-    t.Errorf("not stored properly")
-  }
+	if !reflect.DeepEqual(loaded, ws) {
+		t.Errorf("not stored properly")
+	}
 }
