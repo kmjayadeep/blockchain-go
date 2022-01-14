1bb5ef0dbb7da57ce1e8d842fa297baab8dc2eac
Jayadeep KM <kmjayadeep@gmail.com>
2022-01-14 20:03:17 +0100

    pubkeyhash util
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

---
+++ wallet/wallets.go
@@ -0,0 +1,34 @@
+package wallet
+
+import (
+	"crypto/elliptic"
+	"encoding/gob"
+	"io"
+)
+
+type Wallets struct {
+  Wallets map[string]*Wallet
+}
+
+func NewWallets() *Wallets {
+  return &Wallets{
+    Wallets: make(map[string]*Wallet),
+  }
+}
+
+func LoadWallets(r io.Reader) (*Wallets, error) {
+  ws := &Wallets{}
+  gob.Register(elliptic.P256())
+  decoder := gob.NewDecoder(r)
+  err := decoder.Decode(ws)
+  if err != nil {
+    return nil, err
+  }
+  return ws, nil
+}
+
+func (ws *Wallets) Save(w io.Writer) error {
+  gob.Register(elliptic.P256())
+  encoder := gob.NewEncoder(w)
+  return encoder.Encode(ws)
+}
---
+++ wallet/wallets_test.go
@@ -0,0 +1,41 @@
+package wallet_test
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+
+	"github.com/kmjayadeep/blockchain-go/wallet"
+)
+
+func TestWalletsStore(t *testing.T) {
+  b := bytes.Buffer{}
+
+  ws := wallet.NewWallets()
+  w := wallet.Wallet{
+    PublicKey: []byte("test"),
+  }
+  ws.Wallets["test"] = &w
+
+  err := ws.Save(&b)
+  if err != nil {
+    t.Errorf("Unable to save wallets with error %v", err)
+  }
+
+  loaded, err := wallet.LoadWallets(&b)
+  if err != nil {
+    t.Errorf("Unable to load wallets with error %v", err)
+  }
+
+  if loaded == nil || loaded.Wallets == nil {
+    t.Fatalf("Wallets are nil")
+  }
+
+  if len(loaded.Wallets) == 0 {
+    t.Errorf("Wallets are empty")
+  }
+
+  if !reflect.DeepEqual(loaded, ws) {
+    t.Errorf("not stored properly")
+  }
+}
